1dac84bdf45d2b36a081fb69f92de6966da71cc8
ito-hinata <105197923+ito-hinata@users.noreply.github.com>
2023-07-16 14:22:19 +0900

    １

--- docs/script.js
+++
@@ -1,8 +0,0 @@
-// ボタン要素を取得
-const startButton = document.getElementById('start-button');
-
-// ボタンクリック
-startButton.addEventListener('click', function() {
-    // 新しいページにリダイレクト
-    window.location.href = 'index2.html';
-  });
